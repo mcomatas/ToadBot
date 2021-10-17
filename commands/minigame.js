16e5d476eb012c60bb9e3b96ef0d47dc3944839c
Michael Comatas <mac776@scarletmail.rutgers.edu>
2021-10-17 14:08:42 -0400

    Fixed minigame command

--- commands/minigame.js
+++ commands/minigame.js
@@ -5,11 +5,15 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('minigame')
 		.setDescription('Choose a random minigame to play')
-		.addStringOption(option => option.setName('input').setDescription('4p, 3v1, 2v2, or battle')),
+		.addStringOption(option => option.setName('category').setDescription('4p, 3v1, 2v2, or battle').setRequired(true)
+        .addChoice('4P', 'four')
+        .addChoice('1v3', 'three')
+        .addChoice('2v2', 'two')
+        .addChoice('battle', 'battle')),
 	async execute(interaction) {
-		const value = interaction.options.getString('input');
-        console.log(value);
-		if ( value.toLowerCase() === '4p' )
+		const value = interaction.options.getString('category');
+        // console.log(value);
+        if ( value == 'four' )
         {
             const four = ['Lava Tile Isle', 'Hot Rope Jump', 'Shell Shocked', 'Toad in the Box', 'Mecha-Marathon', 'Roll Call', 'Abandon Ship', 'Plaktform Peril', 'Totem Pole Pound',
                             'Bumper Balls', 'Bombs Away', 'Tipsy Tourney', 'Honeycomb Havoc', 'Hexagon Heat', 'Skateboard Scamper', 'Slot Car Derby', 'Shy Guys Says', 'Sneak \'n\' Snore',
@@ -17,29 +21,27 @@ module.exports = {
             const rand = Math.floor(Math.random() * four.length);
             return interaction.reply( four[rand] );
         }
-        else if ( value.toLowerCase() === '3v1' )
+        else if ( value == 'three' )
         {
             const three = ['Bowl Over', 'Crane Game', 'Move to the Music', 'Bob-omb Barrage', 'Look Away', 'Shock, Drop or Roll', 'Lights Out', 'Filet Relay', 'Archer-ival', 'Quicksand Cache', 
                             'Rainbow Run'];
-            const rand = Math.floor(Math.random * three.length);
+            const rand = Math.floor(Math.random() * three.length);
             return interaction.reply( three[rand] );
         }
-        else if ( value.toLowerCase() === '2v2' )
+        else if ( value == 'two' )
         {
             const two = ['Toad Bandstand', 'Bobsled Run', 'Handcar Havoc', 'Balloon Burst', 'Sky Pilots', 'Speed Hockey', 'Cake Factory', 'Magnet Carta', 'Looney Lumberjacks', 
                         'Torpedo Targets', 'Destruction Duet', 'Dungeon Dash'];
-            const rand = Math.floor(Math.random * two.length);
+            const rand = Math.floor(Math.random() * two.length);
             return interaction.reply( two[rand] );
 
         }
-        else if ( value.toLowerCase() === 'battle' )
+        else if ( value == 'battle' )
         {
             const battle = ['Grab Bag', 'Bumper Balloon Cars', 'Rakin\' em in', 'Day at the Races', 'Face Lift', 'Crazy Cutters', 'Hot Bob-omb', 'Bowser\'s Big Blast'];
-            const rand = Math.floor(Math.random * battle.length);
+            const rand = Math.floor(Math.random() * battle.length);
             return interaction.reply( battle[rand] );
         }
-        
-        // if (value) return interaction.reply(`The options value is: \`${value}\``);
 		return interaction.reply('Put in the right arguments dummy!');
 	},
 };
